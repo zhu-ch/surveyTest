53866f5998f471c124591106165155470c7b4564
yzj <1216460878@qq.com>
2020-09-22 10:59:41 +0800

    新建历史答卷页面，并将其连接到frame页面，在utilJS修改了serverURL

--- utils/utilJS.js
+++ utils/utilJS.js
@@ -106,5 +106,5 @@ function copy(object) {
     return JSON.parse(JSON.stringify(object));
 }
 
-let serverUrl='http://10.4.20.152:8666'
-// let serverUrl = 'http://wxxyx.m0yuqi.cn:8666'
+// let serverUrl='http://10.4.20.152:8666'
+let serverUrl = 'http://wxxyx.m0yuqi.cn:8666'
